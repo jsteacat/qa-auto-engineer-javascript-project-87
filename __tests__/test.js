3cc013770abd7a1ece8f1d62d3f6bd780127d60f
Stepan Kormilin <ubscoder@gmail.com>
2024-01-14 21:43:15 +0300

    cc

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,24 +1,21 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import fs from 'fs';
-// import generateDiffs from '../src/index.js';
+import generateDiffs from '../src/index.js';
 
-// eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
-// eslint-disable-next-line no-underscore-dangle
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-// const expectedStylish = readFile('stylish.txt');
-// const expectedPlain = readFile('plain.txt');
-// const expectedJson = readFile('json.txt');
+const expectedStylish = readFile('stylish.txt');
+const expectedPlain = readFile('plain.txt');
+const expectedJson = readFile('json.txt');
 
 test.each(['json', 'yml'])('%s test', (format) => {
-  // const file1 = getFixturePath(`file1.${format}`);
-  // const file2 = getFixturePath(`file2.${format}`);
-  // expect(generateDiffs(file1, file2, 'stylish')).toEqual(expectedStylish);
-  // expect(generateDiffs(file1, file2, 'plain')).toEqual(expectedPlain);
-  // expect(generateDiffs(file1, file2, 'json')).toEqual(expectedJson);
-  console.log('test!!!');
+  const file1 = getFixturePath(`file1.${format}`);
+  const file2 = getFixturePath(`file2.${format}`);
+  expect(generateDiffs(file1, file2, 'stylish')).toEqual(expectedStylish);
+  expect(generateDiffs(file1, file2, 'plain')).toEqual(expectedPlain);
+  expect(generateDiffs(file1, file2, 'json')).toEqual(expectedJson);
 });
