b1618752fb702dc420a681b60d879e3d6ad73b03
Stepan Kormilin <ubscoder@gmail.com>
2024-01-17 19:53:35 +0300

    fix test

--- src/dataChanges.js
+++ src/dataChanges.js
@@ -3,24 +3,9 @@ export default (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
 
-  function bubbleSort(arr) {
-    for (let i = 0, endI = arr.length - 1; i < endI; i += 1) {
-      let wasSwap = false;
-      for (let j = 0, endJ = endI - i; j < endJ; j += 1) {
-        if (arr[j] > arr[j + 1]) {
-          const swap = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = swap;
-          wasSwap = true;
-        }
-      }
-      if (!wasSwap) break;
-    }
-    return arr;
-  }
-
   // 2: get all unique keys
-  const allKeys = bubbleSort(Array.from(new Set([...keys1, ...keys2])));
+  // eslint-disable-next-line fp/no-mutating-methods
+  const allKeys = Array.from(new Set([...keys1, ...keys2])).sort();
 
   return allKeys.map((key) => {
     if (!keys1.includes(key) && keys2.includes(key)) {
